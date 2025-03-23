05aae216da0b2f7563e1ae158cd7db1aa0065cdc
Arctik-Senior <kozulka2224.popit@gmail.com>
2025-03-23 13:54:05 +0300

    вуа

--- anfisa/posts/forms.py
+++ anfisa/posts/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-from .models import Create  # Импортируем модель Create
+from .models import Post  # Импортируем модель Create
 
 
 class CreateForm(forms.ModelForm):
     class Meta:
-        model = Create  # Указываем модель, с которой связана форма
+        model = Post  # Указываем модель, с которой связана форма
         fields = ('text', 'group')  # Указываем поля, которые будут в форме
