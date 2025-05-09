277a171b9b219e98d190ae73938e1fcc34c8737f
Александр Галушкин <stas.basov@whatever.com>
2025-05-09 20:06:09 +0300

    django-debug-toolbar

--- anfisa/anfisa/settings.py
+++ anfisa/anfisa/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'about.apps.AboutConfig',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +59,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'anfisa.urls'
--- anfisa/anfisa/urls.py
+++ anfisa/anfisa/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     # импорт правил из приложения posts
@@ -33,6 +32,6 @@ urlpatterns = [
 handler404 = 'core.views.page_not_found'
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+    import debug_toolbar
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
